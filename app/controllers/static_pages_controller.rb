ed80fa80e40db738ac5da1af7a1288173a6bf181
Pete <petevonna@lycos.com>
2012-09-22 17:11:21 -0400

    Add css

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,4 +10,8 @@ class StaticPagesController < ApplicationController
   
   def help
   end
+  
+  def shop
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Davepowerman::Application.routes.draw do
   match '/help', to: 'static_pages#help'
   match '/about', to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
+  match '/shop', to: 'static_pages#shop'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
